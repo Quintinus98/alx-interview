dfc596cfb11cea11cfc80725ec6227bae5d3d9ea
David <obododavidsop@gmail.com>
2024-04-28 02:45:57 +0100

    Pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Pascal's triangle implementation with python
+"""
+
+
+def factorial(k):
+    """
+    Factorial formula
+    """
+    if k <= 1:
+        return 1
+    return factorial(k - 1) * k
+
+
+def pascal_formula(n, m):
+    """
+    Pascal's triangle formaula
+    """
+    if n == 0:
+        return 1
+    return factorial(n) // (factorial(n - m) * factorial(m))
+
+
+def pascal_triangle(n):
+    """
+    Create pascal's triangle
+    """
+    pascals = []
+    if n == 0:
+        pascals.append([1])
+        return pascals
+    for k in range(0, n):
+        iter = [pascal_formula(k, i) for i in range(0, k + 1)]
+        pascals.append(iter)
+    return pascals
+
+
+if __name__ == "__main__":
+    print(pascal_triangle())
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,4 @@
+# Pascal's triangle
+
+I found this YouTube video very interesting [Pascal's Triangle - Numberphile](https://www.youtube.com/watch?feature=shared&v=0iMtlus-afo)
+
