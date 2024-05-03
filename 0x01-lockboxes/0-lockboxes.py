cfb6d3c9b32cf0ee98d27ecbdbc01550e1b04510
David <obododavidsop@gmail.com>
2024-05-03 03:20:47 +0100

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,15 +2,15 @@ def canUnlockAll(boxes):
     if not boxes:
         return False
 
-    n = len(boxes)
-    visited = [False] * n
+    num = len(boxes)
+    visited = [False] * num
     visited[0] = True
     queue = [0]
 
     while queue:
         current_box = queue.pop(0)
         for key in boxes[current_box]:
-            if key < n and not visited[key]:
+            if key < num and not visited[key]:
                 visited[key] = True
                 queue.append(key)
 
