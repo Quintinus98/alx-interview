5c692e50a92f818633197443856fe682cbc0608e
David <obododavidsop@gmail.com>
2024-05-17 00:50:12 +0100

    updated log parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,65 +4,49 @@ Log Parsing
 """
 
 import sys
-import signal
-
-totalSize = 0
-statusCode = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0,
-}
 
 
-def log_stats():
+def logstatus(totalSize, statusCode):
     """Logs the stats"""
-    global totalSize, statusCode
     print(f"File size: {totalSize}")
     for code in sorted(statusCode):
         if statusCode[code] > 0:
             print(f"{code}: {statusCode[code]}")
 
 
-def handler(signal, frame):
-    """Signal handler function"""
-    log_stats()
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, handler)
-
-
 def main():
     """Execute main program"""
     count = 0
-    global totalSize, statusCode
+    totalSize = 0
+    statusCode = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
     try:
         for line in sys.stdin:
             parts = line.split()
-            if len(parts) < 9:
-                continue
-            endpoint = parts[4] + " " + parts[5] + " " + parts[6]
-            if endpoint != '"GET /projects/260 HTTP/1.1"':
-                continue
             try:
-                code = int(parts[7])
-                size = int(parts[8])
+                totalSize += int(parts[-1])
             except ValueError:
-                continue
-            if code in statusCode:
-                statusCode[code] += 1
-            totalSize += size
+                pass
+            try:
+                if parts[-2] in statusCode:
+                    statusCode[parts[-2]] += 1
+            except Exception:
+                pass
             count += 1
             if count % 10 == 0:
-                log_stats()
+                logstatus(totalSize, statusCode)
+        logstatus(totalSize, statusCode)
     except KeyboardInterrupt:
-        log_stats()
-        sys.exit(0)
+        logstatus(totalSize, statusCode)
+        raise
 
 
 if __name__ == "__main__":
