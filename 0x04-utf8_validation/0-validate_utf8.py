e5121a82d41ad10d42798cb84ff21ed08bfa4230
David <obododavidsop@gmail.com>
2024-05-24 03:17:31 +0100

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,23 +2,43 @@
 """ UTF-8 Validation """
 
 
-def count_leading_ones(byte):
-    """Counting Leading ones"""
-    for i in range(8):
-        if byte >> (7 - i) == 0b11111111 >> (7 - i) & ~1:
-            return i
-    return 8
-
-
 def validUTF8(data):
-    """Determine if the data is a valid UTF8"""
-    data = iter(data)
-    for leading_byte in data:
-        leading_ones = count_leading_ones(leading_byte)
-        if leading_ones in [1, 7, 8]:
-            return False  # Illegal leading byte
-        for _ in range(leading_ones - 1):
-            trailing_byte = next(data, None)
-            if trailing_byte is None or trailing_byte >> 6 != 0b10:
-                return False  # Missing or illegal trailing byte
-    return True
+    """Determines if a given data set represents a valid UTF-8 encoding."""
+    # Number of bytes in the current UTF-8 character
+    count_bytes = 0
+
+    # Masks for checking the most significant bits
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
+
+    # The data set can contain multiple characters
+    for byte in data:
+        # Handling the 8 least significant bits of each integer (byte)
+        byte = byte & 0xFF
+
+        if count_bytes == 0:
+            # Determine the number of bytes in the UTF-8 character
+            if (byte & mask1) == 0:
+                # 1-byte character
+                continue
+            elif (byte & (mask1 | mask2)) == mask1:
+                # Invalid 10xxxxxx byte in start
+                return False
+            else:
+                # Count the number of leading 1s to determine the length
+                mask = mask1
+                while byte & mask:
+                    count_bytes += 1
+                    mask >>= 1
+
+                # A character in UTF-8 can only be 1 to 4 bytes long
+                if count_bytes == 1 or count_bytes > 4:
+                    return False
+        else:
+            # Check that the byte is of form 10xxxxxx
+            if not (byte & mask1 and not (byte & mask2)):
+                return False
+        count_bytes -= 1
+
+    # All characters should be completely processed
+    return count_bytes == 0
