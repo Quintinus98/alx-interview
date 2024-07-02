6fabf43a776f20b257e7b04d4a8ca87f6bc5a621
David <obododavidsop@gmail.com>
2024-07-02 22:23:09 +0100

    updated prime

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -26,38 +26,82 @@ def isWinner(x, nums):
 
     Return: name of the player that won the most rounds
     """
+    if not nums or x < 1:
+        return None
+
+    pm = findPrime(max(nums))
+    primes = set(pm)
     players = {"Maria": 0, "Ben": 0}
 
     for num in nums:
-        pm = findPrime(num)
-        numlist = [i for i in range(num + 1)]
-        initScores = {"Maria": 0, "Ben": 0}
+        num_set = set(range(1, num + 1))
+        # initial turn
         turn = 0
 
-        for k in range(x):
-            try:
-                choice = pm[0]
-                del pm[0]
-                numlist = [elem for elem in numlist if elem % choice != 0]
-
-                if turn == 0:
-                    initScores["Maria"] += 1
-                    turn = 1
-                else:
-                    initScores["Ben"] += 1
-                    turn = 0
-
-            except IndexError:
+        while True:
+            moves = [p for p in primes if p in num_set]
+            if not moves:
                 break
+            move = moves[0]
 
-        if initScores["Maria"] == initScores["Ben"] and k < x - 1:
-            players["Ben"] += 1
-        if initScores["Maria"] > initScores["Ben"]:
+            # Remove multiples
+            multiples_of_move = set(range(move, num + 1, move))
+            num_set -= multiples_of_move
+
+            turn = 1 - turn
+
+        if turn:
             players["Maria"] += 1
+        else:
+            players["Ben"] += 1
 
-    if players["Maria"] == players["Ben"]:
-        return None
-    if players["Maria"] > players["Ben"]:
-        return "Maria"
     if players["Maria"] < players["Ben"]:
         return "Ben"
+    elif players["Maria"] > players["Ben"]:
+        return "Maria"
+    else:
+        return None
+
+
+# def isWinner(x, nums):
+#     """The player that cannot make a move loses the game.
+
+#     Return: name of the player that won the most rounds
+#     """
+#     if not nums or x < 1:
+#         return None
+
+#     players = {"Maria": 0, "Ben": 0}
+
+#     for num in nums:
+#         pm = findPrime(num)
+#         numlist = [i for i in range(num + 1)]
+#         initScores = {"Maria": 0, "Ben": 0}
+#         turn = 0
+
+#         for k in range(x):
+#             try:
+#                 choice = pm[0]
+#                 del pm[0]
+#                 numlist = [elem for elem in numlist if elem % choice != 0]
+
+#                 if turn == 0:
+#                     initScores["Maria"] += 1
+#                 else:
+#                     initScores["Ben"] += 1
+#                 turn = 1 - turn
+
+#             except IndexError:
+#                 break
+
+#         if initScores["Maria"] == initScores["Ben"] and k < x - 1:
+#             players["Ben"] += 1
+#         if initScores["Maria"] > initScores["Ben"]:
+#             players["Maria"] += 1
+
+#     if players["Maria"] == players["Ben"]:
+#         return None
+#     if players["Maria"] > players["Ben"]:
+#         return "Maria"
+#     if players["Maria"] < players["Ben"]:
+#         return "Ben"
